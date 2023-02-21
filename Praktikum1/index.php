93b9352973bce6795a93a277209248314fc3aa75
Adamyr159 <adamyr159@gmail.com>
2023-02-21 15:13:58 +0700

    upload praktikum 1

---
+++ Praktikum1/index.php
@@ -0,0 +1,64 @@
+<?php 
+
+// ini sebuah komentar
+/* 
+ini sebuah komentar 
+
+Komentar adalah sejenis anotasi yang bisa digunakan untuk mengklarifikasi 
+    tujuan dan maksud satu bagian kode. Ketika menggunakan PHP, ada beberapa opsi 
+    untuk menulis komentar yang dapat dipilih, dan opsi-opsi tersebut diangkat 
+    dari bahasa pemrograman lama yang populer.
+*/
+
+// echo "Hello World <br>";
+// print_r("Nama saya Adam <br>");
+// var_dump("kuliah di STT Terpadu Nurul Fikri");
+
+// Membuat variable user
+$nama = "Adam Yusron";
+$umur = 18;
+$berat = 70.2;
+$mahasiswa = true;
+
+// echo "Nama saya adalah $nama <br>";
+// echo "Umur saya adalah $umur tahun <br>";
+// echo "Berat badan $berat kg <br>";
+
+
+// // Membuat variabel sistem
+// echo "Dokument root " . $_SERVER["DOCUMENT_ROOT"];
+// echo "Nama File " . $_SERVER['PHP_SELF'];
+
+// Membuat variabel konstanta
+// define("PHI", 3.14);
+// $jari = 8;
+// $luas = PHI * $jari * $jari;
+// $keliling = 2 * PHI * $jari;
+
+// echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
+// echo "Keliling lingkaran adalah $keliling";
+
+
+
+// Membuat Array
+$programs = ["php", "Javascript", "HTML", "CSS"];
+// echo $programs[0];
+// echo count($programs);
+// $programs[] = "Mysql";
+// echo count($programs);
+
+// unset($programs[0]);
+// echo "<br>" . $programs[0];
+
+// echo "<ol>";
+// foreach($programs as $program){
+//     echo "<li> $program </li>";
+// }
+// echo "</ol>";
+
+
+
+
+
+
+?>
