c7e87c284523a823f1c494a49451a65b222cdc0f
Adamyr159 <adamyr159@gmail.com>
2023-02-22 15:39:04 +0700

    upload praktikum1

--- Praktikum1/index.php
+++ Praktikum1/index.php
@@ -1,64 +1,53 @@
 <?php 
-
 // ini sebuah komentar
 /* 
-ini sebuah komentar 
-
-Komentar adalah sejenis anotasi yang bisa digunakan untuk mengklarifikasi 
+    ini sebuah komentar 
+    Komentar adalah sejenis anotasi yang bisa digunakan untuk mengklarifikasi 
     tujuan dan maksud satu bagian kode. Ketika menggunakan PHP, ada beberapa opsi 
     untuk menulis komentar yang dapat dipilih, dan opsi-opsi tersebut diangkat 
     dari bahasa pemrograman lama yang populer.
 */
 
-// echo "Hello World <br>";
-// print_r("Nama saya Adam <br>");
-// var_dump("kuliah di STT Terpadu Nurul Fikri");
+# ini sebuah komentar juga
 
-// Membuat variable user
-$nama = "Adam Yusron";
+
+// echo "Hello World <br>";
+// echo 'Hello World <br>';
+// print_r("Adam Yusron <br>");
+// var_dump("STT Nurul Fikri");
+// var_dump(123);
+
+
+// Membuat variabel user
+$nama = "Adam Malik";
 $umur = 18;
-$berat = 70.2;
+$berat = 70.5;
 $mahasiswa = true;
 
-// echo "Nama saya adalah $nama <br>";
-// echo "Umur saya adalah $umur tahun <br>";
-// echo "Berat badan $berat kg <br>";
+// echo "Hallo, Nama saya $nama";
+// echo " Umurku $umur th";
+// echo "dan memiliki berat badan $berat kg";
 
 
-// // Membuat variabel sistem
-// echo "Dokument root " . $_SERVER["DOCUMENT_ROOT"];
-// echo "Nama File " . $_SERVER['PHP_SELF'];
+// Membuat variabel sistem
+// echo "Dokument root " . $_SERVER['DOCUMENT_ROOT'];
+// echo "<br>";
+// echo "Nama file " . $_SERVER['PHP_SELF'];
 
 // Membuat variabel konstanta
-// define("PHI", 3.14);
-// $jari = 8;
-// $luas = PHI * $jari * $jari;
-// $keliling = 2 * PHI * $jari;
+define('PHI', 3.14);
+$jari = 8;
+$luas = PHI * $jari * $jari;
+$keliling = 2 * PHI * $jari;
 
-// echo "Luas lingkaran dengan jari-jari = $jari = $luas <br>";
-// echo "Keliling lingkaran adalah $keliling";
-
-
-
-// Membuat Array
-$programs = ["php", "Javascript", "HTML", "CSS"];
-// echo $programs[0];
-// echo count($programs);
-// $programs[] = "Mysql";
-// echo count($programs);
-
-// unset($programs[0]);
-// echo "<br>" . $programs[0];
-
-// echo "<ol>";
-// foreach($programs as $program){
-//     echo "<li> $program </li>";
-// }
-// echo "</ol>";
+// echo "<br> Luas lingkaran dengan jari-jari $jari = $luas";
+// echo "<br> Luas lingkaran dengan jari-jari $jari = $keliling";
 
 
-
-
+// Membuat array 
+$programs = ["Php", "Javascript", "HTML", "CSS"];
+// echo $programs[3];
+echo "jumlah data sebanyak " . count($programs);
 
 
 ?>
